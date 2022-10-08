5ab29a6c28126318c93986d52859033c3bb3a45c
plzmake <chotaotaodkm2@gmail.com>
2022-10-08 22:02:46 +0700

    first commit

---
+++ plzwin/Core/Inc/lab_2.h
@@ -0,0 +1,17 @@
+/*
+ * lab_2.h
+ *
+ *  Created on: Oct 7, 2022
+ *      Author: Admin
+ */
+
+#ifndef INC_LAB_2_H_
+#define INC_LAB_2_H_
+
+extern int Timer1_flag;
+void Settimer1(int du);
+extern int Timer2_flag;
+void Settimer2(int du);
+void TimerRun();
+
+#endif /* INC_LAB_2_H_ */
---
+++ plzwin/Debug/Core/Src/lab_2.d
@@ -0,0 +1,3 @@
+Core/Src/lab_2.o: ../Core/Src/lab_2.c ../Core/Inc/lab_2.h
+
+../Core/Inc/lab_2.h:
